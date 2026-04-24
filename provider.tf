fd6c7101504c100368d9c6d6a334334425d3bdc1
yaswanthmavuri <yaswanthmavuri232@gmail.com>
2026-04-24 10:46:26 +0530

    Added remote state backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+  
+  # Ikkada kothaga Backend block pedutunnam
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rgg"            # Nuvvu create chesina RG name
+    storage_account_name = "yashtfstate3249"   # Nuvvu pettina unique storage name
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"     # Ee peru thone file save avtundi
+  }
 }
 
 provider "azurerm" {
